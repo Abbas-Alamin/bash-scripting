8a329d395e9a7fa0f6a4667953a12ec5025af08c
Abbas Alamin <3bbasalamin.work@gmail.com>
2026-07-04 23:15:29 +0300

    Delete monitor-space.sh

--- monitor-space.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-danger=80
-usage="df / | awk 'NR==2 {print$5}' | sed 's/%//' "
-if [ $usage -gt $danger ]
-then
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "WARNING!!! SPACE IN (/) HAS EXCEDED CRITICAL LEVELS!!!!"
-  echo " currnet usage : $usage more than $danger"
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
